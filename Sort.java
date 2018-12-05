0e759107c3e311ade08b7a4c797c82973856d508
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-12-04 19:52:42 -0500

    selectionsort and Driver edited!

--- Sort.java
+++ Sort.java
@@ -1,12 +1,26 @@
 public class Sort {
 
   public static void selectionsort(int[] ary) {
-
+    int index = ary.length - 1;
+    for(int i = index; i >= 0; i--) {
+      int max = 0;
+      int place = 0;
+      int num = 0;
+      for(int j = i; j >= 0; j--) {
+        if(max < ary[j]) {
+          max = ary[j];
+          place = j;
+          num = ary[i];
+        }
+      }
+      ary[i] = max;
+      ary[place] = num;
+    }
   }
 
   public static void main(String[] args) {
-    int[] data;
-    System.out.println();
+    int[] data = new int[] {15,3,67,13};
+    System.out.println(data);
   }
 
 }
