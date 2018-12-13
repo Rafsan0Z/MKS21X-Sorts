3664ffd55d7c3f9e1e5a229445c3d7728e1caf2d
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-12-13 11:44:30 -0500

    deleting defective selectionSort

--- Sorts.java
+++ Sorts.java
@@ -1,24 +1,6 @@
 import java.util.Random;
 public class Sorts {
 
-  public static void selectionsort(int[] ary) {
-    int index = ary.length - 1;
-    for(int i = index; i >= 0; i--) {
-      int max = 0;
-      int place = 0;
-      int num = 0;
-      for(int j = i; j >= 0; j--) {
-        if(max < ary[j]) {
-          max = ary[j];
-          place = j;
-          num = ary[i];
-        }
-      }
-      ary[i] = max;
-      ary[place] = num;
-    }
-  }
-
   public static void selectionSort(int[] ary) {
     int index = ary.length - 1;
     for(int i = 0; i <= index; i++ ) {
